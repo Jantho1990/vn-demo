0e6481fa9a2f2d26445bc202fb1f665a29bdc29d
Joshua Anthony <jantho1990@gmail.com>
2018-02-15 20:30:38 -0600

    changed to express

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(80, function () {
-    console.log('Server running on 80...');
+var express = require('express');
+
+// Create our app.
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
+    }
+});
+
+app.use(express.static('public'));
+
+app.listen(port, function () {
+    console.log('Server fired up on', port);
 });
