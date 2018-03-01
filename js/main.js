6c91a176ca5dabe7cb5bce5d9eb57f7965fed12f
Joshua Anthony <jantho1990@gmail.com>
2018-02-28 22:19:18 -0600

    added a visual on the bottom to indicate when there is unread text (though right now it just indicates when scroll is not at the bottom

--- js/main.js
+++ js/main.js
@@ -3,3 +3,56 @@
  * Your Javascript Code Goes Here
  * ==============================
  **/
+
+// Forgive me, this code is disgusting. :(
+
+/**
+ * Listen to the data-ui-text scroll.
+ */
+(function listenToScroll(document) {
+	console.log('i ran');
+	document.addEventListener('DOMContentLoaded', function () {
+		ooo()
+	})
+
+	function ooo() {
+		document.querySelector('[data-ui="text"]').addEventListener('scroll', function (ev) {
+			bottomBar(ev)
+		})
+		document.querySelector('[data-ui="text"]').addEventListener('mousedown', function (ev) {
+			console.log('bongos');
+            // window.setTimeout(bottomBar(ev, true), 200)
+            bottomBarClick(ev)
+			console.log('sniggers')
+		})
+	}
+
+	function bottomBarClick(ev) {
+        let bottom = ev.target.scrollHeight
+        let height = ev.target.clientHeight
+        let pos = ev.target.scrollTop
+        let p = height + pos
+        console.log('bongos', 'bottom', bottom, 'height', height, 'pos', pos, 'p', p)
+        // if (bottom > height) {
+			document.querySelector('[data-ui="text"').classList.add('unread')
+		// }
+	}
+
+	function bottomBar(ev) {
+		// console.log(ev);
+		let bottom = ev.target.scrollHeight
+		let height = ev.target.clientHeight
+		let pos = ev.target.scrollTop
+		let p = height + pos
+        console.log('bottom', bottom, 'height', height, 'pos', pos, 'p', p)
+        if (bottom > height) {
+			if (p === bottom && ev.target.classList.contains('unread')) {
+				ev.target.classList.remove('unread')
+			} else if (p !== bottom && ev.target.classList.contains('unread') === false) {
+				console.log('chimichangas')
+				ev.target.classList.add('unread')
+			}
+		}
+	}
+
+})(document)
